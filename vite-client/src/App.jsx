f443b777ee7f59290224eebe7bea7bf816a335e0
Dahye Won <doall1102@gmail.com>
2024-04-12 21:03:26 +0900

    client

---
+++ vite-client/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Movies from "./routes/movies"
+import Movie from "./routes/movie"
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Movies />} />
+      <Route path="/movies/:id" element={<Movie />} />
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
